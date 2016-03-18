d68d03d158b697871106dff8d2d64ddb57d0acce
Brooke Hurford <brookebhurford@gmail.com>
2016-03-18 09:02:04 -0700

    Make search field work in single js file for number of public repos

--- build/js/app.js
+++ build/js/app.js
@@ -1,7 +1,22 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "4814bf6eccf203b472de472aedcf9dd16c5cc922";
+
+},{}],2:[function(require,module,exports){
+// var getRepo = require('./../js/github.js').getRepo;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
-  $('#time').text(moment());
+  $('form#githubSearch').submit(function(event){
+    event.preventDefault();
+    var inputtedUsername = $('input#username').val();
+
+    $.get('https://api.github.com/users/'+ inputtedUsername + '?access_token=' + apiKey).then(function(response){
+      console.log(response);
+      $('.searchResults').text(response.public_repos);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  })
 });
 
-},{}]},{},[1]);
+},{"./../.env":1}]},{},[2]);
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,3 +1,16 @@
+// var getRepo = require('./../js/github.js').getRepo;
+var apiKey = require('./../.env').apiKey;
+
 $(document).ready(function() {
-  $('#time').text(moment());
+  $('form#githubSearch').submit(function(event){
+    event.preventDefault();
+    var inputtedUsername = $('input#username').val();
+
+    $.get('https://api.github.com/users/'+ inputtedUsername + '?access_token=' + apiKey).then(function(response){
+      console.log(response);
+      $('.searchResults').text(response.public_repos);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  })
 });
