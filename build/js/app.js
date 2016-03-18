3d89fb82ae80cc34715f319b56e6950189cadc0f
Brooke Hurford <brookebhurford@gmail.com>
2016-03-18 11:08:01 -0700

    Show all repos, not just first 30

--- build/js/app.js
+++ build/js/app.js
@@ -6,11 +6,12 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(){
   var inputtedUsername = $('input#username').val();
-  $.get('https://api.github.com/users/'+ inputtedUsername + '/repos?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/'+ inputtedUsername + '/repos?access_token=' + apiKey + '&per_page=1000').then(function(response){
     console.log(response);
     for (var i = 0; i <= response.length; i++) {
       $('ul.resultsList').append('<li>' + '<span class="fullName">' + response[i].full_name + '</span>' + '<br>' +
-                                  response[i].description +
+                                  response[i].description + '<br>' +
+                                  '<a class="button" target="_blank" href="' + response[i].html_url + '">View Repo</a>' +
                                  '</li>');
     };
   }).fail(function(error){
@@ -28,9 +29,5 @@ $(document).ready(function() {
     var newRepo = new getRepos();
   })
 });
-
-// for (var 0 = i; i > response.length; i++){
-//   $('#searchResults').append(response[i].description);
-// }
 
 },{"./../.env":1,"./../js/github.js":2}]},{},[3]);
--- js/github.js
+++ js/github.js
@@ -2,11 +2,12 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(){
   var inputtedUsername = $('input#username').val();
-  $.get('https://api.github.com/users/'+ inputtedUsername + '/repos?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/'+ inputtedUsername + '/repos?access_token=' + apiKey + '&per_page=1000').then(function(response){
     console.log(response);
     for (var i = 0; i <= response.length; i++) {
       $('ul.resultsList').append('<li>' + '<span class="fullName">' + response[i].full_name + '</span>' + '<br>' +
-                                  response[i].description +
+                                  response[i].description + '<br>' +
+                                  '<a class="button" target="_blank" href="' + response[i].html_url + '">View Repo</a>' +
                                  '</li>');
     };
   }).fail(function(error){
