36e5631d3117cd95b1a090a4934414f9e7154424
Brooke Hurford <brookebhurford@gmail.com>
2016-03-18 12:21:54 -0700

    Fix styling issue on list and clear results on a new search

--- build/js/app.js
+++ build/js/app.js
@@ -8,11 +8,10 @@ exports.getRepos = function(){
   var inputtedUsername = $('input#username').val();
   $.get('https://api.github.com/users/'+ inputtedUsername + '/repos?access_token=' + apiKey + '&per_page=1000').then(function(response){
     console.log(response);
-    for (var i = 0; i <= response.length; i++) {
-      $('ul.resultsList').append('<li>' + '<span class="fullName">' + response[i].full_name + '</span>' + '<br>' +
-                                  response[i].description + '<br>' +
-                                  '<a class="button" target="_blank" href="' + response[i].html_url + '">View Repo</a>' +
-                                 '</li>');
+    for (var i = 0; i < response.length; i++) {
+      $('ul.resultsList').append('<ul class="resultsItem"><li>' + '<span class="fullName">' + response[i].full_name + '</span></li>' +
+                                 '<li>' + response[i].description + '</li>' +
+                                 '<li><a class="button" target="_blank" href="' + response[i].html_url + '">View Repo</a></li></ul>');
     };
   }).fail(function(error){
     console.log(error.responseJSON.message);
@@ -25,6 +24,7 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('form#githubSearch').submit(function(event){
+    $('ul.resultsList').empty();
     event.preventDefault();
     var newRepo = new getRepos();
   })
--- js/browser-interface.js
+++ js/browser-interface.js
@@ -3,6 +3,7 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('form#githubSearch').submit(function(event){
+    $('ul.resultsList').empty();
     event.preventDefault();
     var newRepo = new getRepos();
   })
